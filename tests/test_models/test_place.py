2c56c4bc961f1c01ec52d3c92114d829f8ed9048
acidicForest0413 <112514043+acidicForest0413@users.noreply.github.com>
2023-03-28 21:58:31 -0500

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
-from models.place import Place
+from models.review import Review
 
 
-class test_Place(test_basemodel):
+class test_review(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
-        self.name = "Place"
-        self.value = Place
+        self.name = "Review"
+        self.value = Review
